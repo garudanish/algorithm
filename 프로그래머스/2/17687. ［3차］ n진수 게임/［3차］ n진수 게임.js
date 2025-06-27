1fbd836b390403cfa868f591a906009019d4f302
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2025-06-27 09:24:02 +0900

    [level 2] Title: [3차] n진수 게임, Time: 144.69 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,11 +1,15 @@
+// function solution(n, t, m, p) {
+//     const numbers = [];
+//     let currentNumber = 0;
+    
+//     while (numbers.length < t * m) {
+//         numbers.push(...currentNumber.toString(n).toUpperCase().split(""));
+//         currentNumber += 1;
+//     }
+    
+//     return numbers.filter((_, i) => i % m === p - 1).slice(0, t).join("");
+// }
+
 function solution(n, t, m, p) {
-    const numbers = [];
-    let currentNumber = 0;
-    
-    while (numbers.length < t * m) {
-        numbers.push(...currentNumber.toString(n).toUpperCase().split(""));
-        currentNumber += 1;
-    }
-    
-    return numbers.filter((_, i) => i % m === p - 1).slice(0, t).join("");
+    return new Array(t * m).fill(0).flatMap((_, i) => i.toString(n).toUpperCase().split("")).filter((_, i) => i % m === p - 1).slice(0, t).join("");
 }
