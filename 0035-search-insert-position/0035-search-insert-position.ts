80855b3439df24c154ff5190aaa902062a107628
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2025-09-03 16:00:38 +0900

    Time: 0 ms (100%), Space: 56.3 MB (9.05%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -0,0 +1,19 @@
+function searchInsert(nums: number[], target: number, startIndex = 0, lastIndex = nums.length - 1): number {
+    if (startIndex > lastIndex) {
+        return startIndex;
+    }
+
+    const middleIndex = Math.floor((startIndex + lastIndex) / 2);
+
+    const middleIndexElement = nums[middleIndex];
+
+    if (middleIndexElement === target) {
+        return middleIndex;
+    }
+
+    if (middleIndexElement < target) {
+        return searchInsert(nums, target, middleIndex + 1, lastIndex);
+    }
+
+    return searchInsert(nums, target, startIndex, middleIndex - 1);
+};
