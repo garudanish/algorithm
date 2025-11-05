dfbaf536c390fad8dc2f618dd22ac0917257565e
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2025-11-05 17:24:06 +0900

    Time: 18 ms (59.17%), Space: 81.1 MB (5.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -2,10 +2,7 @@ function containsDuplicate(nums: number[]): boolean {
     const map = new Map();
 
     for (const num of nums) {
-        if (map.has(num)) {
-            return true;
-        }
-
+        if (map.has(num)) return true;
         map.set(num, true);
     }
 
